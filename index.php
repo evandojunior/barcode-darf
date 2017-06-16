3bf7b474d198cd56444ad2af5015310d8fa55be5
Evando Junior <evando.junior@live.com>
2017-06-16 17:14:58 -0300

    Upload files

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
-<?php
-use App\BarcodeRender;
-
-require_once 'vendor/autoload.php';
-
-$html = file_get_contents('example_one.html');
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <center>
+      <?php
+      use App\BarcodeRender;
+      require_once 'vendor/autoload.php';
+      $example_one = file_get_contents('example_one.html');
+      BarcodeRender::parse($example_one);
+      echo '<hr>';
+      $example_two = file_get_contents('example_two.html');
+      BarcodeRender::parse($example_two);
+      ?>
+    </center>
+  </body>
+</html>
--- src/BarcodeRender.php
+++ src/BarcodeRender.php
@@ -2,6 +2,71 @@
 
 namespace App;
 
+use Picqer\Barcode\BarcodeGeneratorPNG as BarcodeGenerator;
+
 class BarcodeRender
 {
+    public static function parse($html)
+    {
+        $array = [];
+
+        if (preg_match("/linha = \"(.*)\"/", $html, $array)) {
+            $barcode = preg_replace("/[^0-9]/", "", $array[0]);
+        } elseif (preg_match('/(<td class="titulo" align="center"[^>]*>)(.*?)(<\/td>)/', $html, $array)) {
+            $barcode = preg_replace("/[^0-9]/", "", $array[0]);
+        }
+        $response = self::replaceBarcode(self::generate($barcode), $html);
+        echo self::setStyle($response);
+    }
+
+    public static function generate($barcode)
+    {
+        $generator = new BarcodeGenerator;
+        return '<img width="330" height="50" src="data:image/png;base64,' . base64_encode($generator->getBarcode($barcode, $generator::TYPE_INTERLEAVED_2_5_CHECKSUM)) . '">';
+    }
+
+    public static function getBarcode($html)
+    {
+        $code = [];
+        preg_match("/linha = \"(.*)\"/", $html, $code);
+      // if (!count($code) > 0) {
+      //     throw new \Exception("Barcode nao localizado", 500);
+      // }
+      return $code[1];
+    }
+
+    public static function setStyle($html)
+    {
+        $style = "
+        <style>
+            table {
+                margin-top: -3px;
+            }
+
+            tr > td {
+                padding: 0px; !important
+                word-break:break-all; !important
+                table-layout: fixed; !important
+            }
+
+            .Boleto {
+                font-family: Arial, Helvetica, sans-serif;
+                font-size: 7px;
+                font-weight: bold;
+                font-style: normal;
+            }
+
+
+        </style>";
+
+        $html = str_replace('<table border="1" bordercolor="E1E1E1" width="650">', '<table border="1" bordercolor="E1E1E1" width="520" cellpadding="1" cellspacing="0">', $html);
+        $html = str_replace('<img width="330"', '<img style="margin:2px;padding-left:360px;" width="340"', $html);
+        $html .= $style;
+        return $html;
+    }
+
+    public static function replaceBarcode($barcode, $html)
+    {
+        return preg_replace("/<td align=\"center\" class=\"Boleto\"[^>]*>.*?<\/td>/", '<td align="center">'.$barcode.'</td>', $html);
+    }
 }
